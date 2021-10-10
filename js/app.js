115ae32f1083d4b69e03c5322d13d1d44bba1b28
Emmanuel Lopez <jesusemm010@hotmail.com>
2021-10-09 23:44:31 -0500

    Validando form y mostrando alerta

--- js/app.js
+++ js/app.js
@@ -18,8 +18,30 @@ function saveClient() {
     const camposVacios = [mesa, hora].some(campo => campo === "");
 
     if(camposVacios){
-        imprimirAlerta("Si hay al menos un campo vacío")
+        imprimirAlerta("Ambos campos son obligatorios")
         return;
     }
+
+
+    
+}
+
+function imprimirAlerta(mensaje){
+
+    const verify = document.querySelector(".invalid-feedback");
+
+    if(!verify){
+        const form = document.querySelector(".modal-body form");
+        const alerta = document.createElement("div");
+        alerta.classList.add("invalid-feedback", "d-block", "text-center");
+        alerta.textContent = mensaje;
+
+        form.appendChild(alerta);
+
+        setTimeout( ()=>{
+            alerta.remove();
+        },3000)
+    }
+
     
 }
